3cadd5a0e43673a7876fe95466cbaae438bf8ab5
malloryrbush <malloryrbush@hotmail.com>
2018-06-09 18:14:39 -0400

    first commit

---
+++ VPShelter/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPShelter
+{
+    abstract class Employee
+    {
+        //FIELDS/PROPERTIES
+        public int EmployeeNum { get; set; }
+
+        //METHODS
+        public void FeedPets()
+        {
+
+        }
+
+        public void WaterPets()
+        {
+
+        }
+
+        public void ViewStatus()
+        {
+
+        }
+
+        public void PlayWithPets()
+        {
+
+        }
+
+        //ABSTRACT METHODS
+        public abstract void ClockOut();
+
+        public abstract void Adopt();
+
+    }
+
+    
+
+}
---
+++ VPShelter/VirtualPetShelter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPShelter
+{
+    class VirtualPetShelter
+    {
+        List<VirtualPet> pets = new List<VirtualPet>();
+        List<Employee> employees = new List<Employee>();
+        List<Volunteer> volunteers = new List<Volunteer>();
+        List<Manager> managers = new List<Manager>();
+
+
+        public void SetVirtualPet(VirtualPet pets)
+        {
+            pets.Add(pets);
+        }
+        public void SetEmployee(Employee employees)
+        {
+            employees.Add(employees);
+        }
+        public void SetVolunteer(Volunteer volunteers)
+        {
+            volunteers.Add(volunteers);
+        }
+        public void SetManager(Manager managers)
+        {
+            managers.Add(managers);
+        }
+
+        //Getting the objects is the list 
+        public List<VirtualPet> GetVirtualPet()
+        {
+            return pets;
+        }
+        public List<Employee> GetEmployee()
+        {
+            return employees;
+        }
+        public List<Volunteer> GetVolunteer()
+        {
+            return volunteers;
+        }
+        public List<Manager> GetManager()
+        {
+            return managers;
+        }
+    }
+}
